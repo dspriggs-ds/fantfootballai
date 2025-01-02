63dff8c7a3a6892a2e41a03a1005ada15c0ebc3f
dspriggs <dragoncrane65@gmail.com>
2025-01-02 12:34:01 -0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv, dotenv_values 
+from flask_bootstrap import Bootstrap5, SwitchField
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from openai import AzureOpenAI
+from wtforms import SubmitField, SelectField
+from wtforms.validators import InputRequired
+import data
+import openai
+import json
+import markdown
+import nfl_data_py as nfl
+import numpy as np
+import os
+import pandas as pd
+
+
+app = Flask(__name__) 
+app.secret_key = b'_53oi3uriq9pifpff;apl'
+bootstrap = Bootstrap5(app)
+
+class fantballForm(FlaskForm):
+    week_list = [i for i in range(1,19)]
+    players_df = data.get_players()
+    players_list = [(g.gsis_id, g.display_name) for g in players_df.itertuples()]
+    weeklist = SelectField(u'Season Week', choices = week_list, validators = [InputRequired()])
+    playerslist = SelectField(u'Player', choices = players_list, validators = [InputRequired()])
+    submit = SubmitField('Submit')
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    form = fantballForm()
+ 
+    if form.validate_on_submit():
+        week = form.weeklist.data
+        player_id =form.playerslist.data
+        player_name = data.get_player_name(player_id)
+        ret_value = markdown.markdown(data.get_analsysis(player_id, player_name, week))
+        return render_template('display.html',analysis_text=ret_value)
+    
+    return render_template('index.html', form=form,title='Fantasy Football Analysis') 
+
+@app.route("/about", methods=['GET'])
+def about():
+    return render_template('about.html',title='About')
+
+if __name__ == '__main__': 
+    app.run() 
---
+++ data.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from dotenv import load_dotenv, dotenv_values 
+from openai import AzureOpenAI
+import nfl_data_py as nfl
+import openai
+import os
+
+
+
+def get_players():
+    player_df = nfl.import_players()
+    player_df = player_df[player_df['status']=='ACT']
+    player_df = player_df[['display_name','gsis_id']]
+    player_df = player_df.sort_values(by='display_name')
+    player_df = player_df.reset_index(drop=True)
+    return player_df
+
+def get_player_name(player_id):
+    player_df = nfl.import_players()
+    player_df = player_df[player_df['gsis_id']==player_id]
+    player_name = player_df['display_name'].values[0]
+    return player_name
+
+def get_analsysis(player_id, player_name,week):
+    load_dotenv() 
+    openaiurl = os.getenv('openaiendpoint')
+    openaikey = os.getenv('openaikey')
+
+    openai.api_type= "azure"
+    openai.api_key = openaikey
+    openai.base_url = openaiurl
+  
+
+    
+    client = AzureOpenAI(
+        api_key=openaikey,
+        azure_endpoint=openaiurl,
+        api_version="2024-08-01-preview"
+    )
+    
+    # Create the list of seasons to download
+    seasons = datetime.now().year[2024]
+
+
+    teams_df = nfl.import_team_desc()
+    weekly_data = nfl.import_weekly_data(seasons)
+
+    drop_columns=['fantasy_points', 'fantasy_points_ppr','headshot_url']
+    weekly_data = weekly_data.drop(drop_columns,axis=1)
+    weekly_data=weekly_data[weekly_data['player_id']==  player_id]
+
+ 
+    content = weekly_data.to_json(orient="records")
+
+    prompt = "Provide Fantasy Football Analysis for {0} and Fantasy Football predicted score for Week {1} using the attached data in json format".format(player_name,week)
+
+    response = client.chat.completions.create(
+        model="fantfootball",
+        messages=[
+            {
+                "role": "system", 
+                "content": "You are a Fantasy Football expert."
+            },
+            {
+                "role": "user",
+                "content": content+"\n\n"+prompt,
+            },
+        ],
+    )
+    return response.choices[0].message.content  
+
+
+# player_id = '00-0034796'
+# player_name = get_player_name(player_id)
+# week = 17
+# retvalue = get_analsysis(player_id, player_name,week)
+# print(retvalue)
